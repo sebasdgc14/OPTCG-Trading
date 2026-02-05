4bdaa0c9f9a9252d6a067688ad72a042c1bf4320
sebasdgc14 <sebasdgc14@gmail.com>
2026-02-05 15:36:50 -0500

    Basic homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "components/Home";
+import Login from "components/login";
 import NotFound from "components/NotFound";
-
+import ProtectedPage from "components/Protected";
 function App() {
   return (
     <Router>
@@ -10,6 +11,8 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/users/login" element={<Login />} />
+            <Route path="/profile" element={<ProtectedPage />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
