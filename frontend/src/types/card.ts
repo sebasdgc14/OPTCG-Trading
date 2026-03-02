e2bbe9d58a6b6ffb0edaad4fccf5ca898ed7dca3
sebasdgc14 <sebasdgc14@gmail.com>
2026-03-02 09:41:46 -0500

    feat(types): Added card type

---
+++ frontend/src/types/card.ts
@@ -0,0 +1,18 @@
+export type Card = {
+  //db_id: number;
+  card_id: string;
+  unique_id: string;
+  unique_img_link: string;
+  name: string;
+  rarity: string;
+  set_type: string;
+  print_set: string;
+  card_type: string;
+  color: string;
+  block: string;
+  attribute: string;
+  power: string;
+  cost: string;
+  counter: string;
+  effect: string;
+};
